f9064d0312ab2bee167dd87b3cafdd88a508c66c
Iurii Malchenko <imalchenko@arazoo.com>
2018-10-31 22:27:06 +0200

    example

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
 name := "idea-test-red"
 scalaVersion := "2.12.7"
-scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
-libraryDependencies += "com.chuusai" %% "shapeless" % "2.3.3"
+scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-Ypartial-unification")
+libraryDependencies += "org.typelevel" %% "cats-core" % "1.4.0"
--- src/main/scala/idea/package.scala
+++ src/main/scala/idea/package.scala
@@ -1,35 +1,11 @@
-import shapeless.tag
-import shapeless.tag.@@
+import cats.implicits._
+import cats.data._
 
 package object idea {
 
-  trait TypedId[T] {
-    type Id = String @@ T
-  }
+  val v1: ValidatedNel[String, Int] = 1.validNel
+  val v2: ValidatedNel[String, Int] = 2.validNel
 
-  def newId[T](): String @@ T = tag[T][String]("something")
-
-  case class Test1(
-    id: Test1.Id = newId(),
-  )
-  case class Test2(
-    id: Test2.Id = newId[Test2](),
-  )
-
-  object Test1 extends TypedId[Test1]
-  object Test2 extends TypedId[Test2]
-
-
-  val test1 = Test1()
-
-
-
-  val test2 = Test2()
-
-  def testFn1(id: Test1.Id = newId()): Unit = { }
-  def testFn2(id: Test1.Id = newId[Test1]()): Unit = { }
-
-  testFn1()
-  testFn2()
+  (v1, v2).mapN { case (x1, x2) =>  x1 + x2 }
 
 }
